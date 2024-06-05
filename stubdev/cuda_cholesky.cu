f3d07ef29bc41bc3e64e9a7862be49258b4040c4
Json Z <zzhou292@wisc.edu>
2024-06-05 15:02:22 -0400

    gtest fix and optimization to stride inside the kernel

--- stubdev/cuda_cholesky.cu
+++ stubdev/cuda_cholesky.cu
@@ -17,9 +17,10 @@ static void HandleError(cudaError_t err, const char* file, int line) {
 // =====================
 
 // Cholesky factorization kernel
-__global__ void cholesky_factorization(double* M, double* L,
-                                       size_t num_equations, size_t n,
-                                       size_t offset) {
+__global__ void cholesky_factorization_warp_primitive(double* M, double* L,
+                                                      size_t num_equations,
+                                                      size_t n,
+                                                      size_t num_stride) {
   int equ_idx = blockIdx.x;
   int thread_idx = threadIdx.x;
 
@@ -30,28 +31,88 @@ __global__ void cholesky_factorization(double* M, double* L,
   Eigen::Map<Eigen::MatrixXd> d_M(M + equ_idx * n * n, n, n);
   Eigen::Map<Eigen::MatrixXd> d_L(L + equ_idx * n * n, n, n);
 
-  int j = thread_idx + offset;
-  if (j >= n) return;
+  for (int stride = 0; stride < num_stride; stride++) {
+    int j = thread_idx + stride * 32;
+    if (j >= n) return;
 
-  for (int i = 0; i <= j; ++i) {
-    __syncwarp();
+    double diagonal_val = 0.0;
+    double sum = 0.0;
 
-    if (i == j) {
-      double sum = 0.0;
-      for (int k = 0; k < i; ++k) {
-        sum += d_L(i, k) * d_L(i, k);
-      }
-      d_L(i, i) = sqrt(d_M(i, i) - sum);
-    }
+    for (int i = 0; i <= j; ++i) {
+      sum = 0.0;
 
-    __syncwarp();
-
-    if (j > i) {
-      double sum = 0.0;
       for (int k = 0; k < i; ++k) {
         sum += d_L(j, k) * d_L(i, k);
       }
-      d_L(j, i) = (d_M(j, i) - sum) / d_L(i, i);
+
+      if (i == j) {
+        diagonal_val = sqrt(d_M(i, i) - sum);
+        // d_L(i, i) = diagonal_val;
+      }
+
+      //__syncwarp();
+
+      // diagonal_val = d_L(i, i);
+
+      if (i > stride * 32) {
+        __shfl_sync(0xFFFFFFFF, diagonal_val, i - stride * 32 - 1);
+      }
+
+      if (i <= stride * 32) {
+        diagonal_val = d_L(i, i);
+      }
+
+      // if (i < offset) {
+      //   diagonal_val = d_L(i, i);
+      // }
+
+      if (j > i) {
+        diagonal_val = (d_M(j, i) - sum) / diagonal_val;
+      }
+
+      d_L(j, i) = diagonal_val;
+      __syncwarp();
+    }
+  }
+}
+// Cholesky factorization kernel
+__global__ void cholesky_factorization(double* M, double* L,
+                                       size_t num_equations, size_t n,
+                                       size_t num_stride) {
+  int equ_idx = blockIdx.x;
+  int thread_idx = threadIdx.x;
+
+  if (equ_idx >= num_equations) {
+    return;
+  }
+
+  Eigen::Map<Eigen::MatrixXd> d_M(M + equ_idx * n * n, n, n);
+  Eigen::Map<Eigen::MatrixXd> d_L(L + equ_idx * n * n, n, n);
+
+  for (int stride = 0; stride < num_stride; stride++) {
+    int j = thread_idx + stride * 32;
+    if (j >= n) return;
+
+    for (int i = 0; i <= j; ++i) {
+      __syncwarp();
+
+      if (i == j) {
+        double sum = 0.0;
+        for (int k = 0; k < i; ++k) {
+          sum += d_L(i, k) * d_L(i, k);
+        }
+        d_L(i, i) = sqrt(d_M(i, i) - sum);
+      }
+
+      __syncwarp();
+
+      if (j > i) {
+        double sum = 0.0;
+        for (int k = 0; k < i; ++k) {
+          sum += d_L(j, k) * d_L(i, k);
+        }
+        d_L(j, i) = (d_M(j, i) - sum) / d_L(i, i);
+      }
     }
   }
 }
@@ -59,7 +120,7 @@ __global__ void cholesky_factorization(double* M, double* L,
 // Cholesky solve forward substitution kernel
 __global__ void cholesky_solve_forward(double* L, double* b, double* x,
                                        double* y, size_t num_equations,
-                                       size_t n, size_t offset) {
+                                       size_t n, size_t num_stride) {
   int equ_idx = blockIdx.x;
   int thread_idx = threadIdx.x;
 
@@ -72,30 +133,32 @@ __global__ void cholesky_solve_forward(double* L, double* b, double* x,
   Eigen::Map<Eigen::VectorXd> d_x(x + equ_idx * n, n, 1);
   Eigen::Map<Eigen::VectorXd> d_y(y + equ_idx * n, n, 1);
 
-  int i = thread_idx + offset;
-  if (i >= n) return;
+  for (int stride = 0; stride < num_stride; stride++) {
+    int i = thread_idx + stride * 32;
+    if (i >= n) return;
 
-  // Forward substitution to solve L * y = b
+    // Forward substitution to solve L * y = b
 
-  double sum = 0.0;
-  for (int j = 0; j <= i; ++j) {
-    if (j == i) {
-      d_y(i) = (d_b(i) - sum) / d_L(i, i);
+    double sum = 0.0;
+    for (int j = 0; j <= i; ++j) {
+      if (j == i) {
+        d_y(i) = (d_b(i) - sum) / d_L(i, i);
+      }
+      __syncwarp();
+
+      if (j < i) {
+        sum += d_L(i, j) * d_y(j);
+      }
+
+      __syncwarp();
     }
-    __syncwarp();
-
-    if (j < i) {
-      sum += d_L(i, j) * d_y(j);
-    }
-
-    __syncwarp();
   }
 }
 
 // Cholesky solve backward substitution kernel
 __global__ void cholesky_solve_backward(double* L, double* b, double* x,
                                         double* y, size_t num_equations,
-                                        size_t n, size_t offset) {
+                                        size_t n, size_t num_stride) {
   int equ_idx = blockIdx.x;
   int thread_idx = threadIdx.x;
 
@@ -108,21 +171,23 @@ __global__ void cholesky_solve_backward(double* L, double* b, double* x,
   Eigen::Map<Eigen::VectorXd> d_x(x + equ_idx * n, n, 1);
   Eigen::Map<Eigen::VectorXd> d_y(y + equ_idx * n, n, 1);
 
-  int i = n - 1 - (thread_idx + offset);
-  if (i < 0) return;
+  for (int stride = 0; stride < num_stride; stride++) {
+    int i = n - 1 - (thread_idx + stride * 32);
+    if (i < 0) return;
 
-  double sum = 0.0;
-  for (int j = n - 1; j >= i; --j) {
-    if (j == i) {
-      d_x(i) = (d_y(i) - sum) / d_L(i, i);
+    double sum = 0.0;
+    for (int j = n - 1; j >= i; --j) {
+      if (j == i) {
+        d_x(i) = (d_y(i) - sum) / d_L(i, i);
+      }
+      __syncwarp();
+
+      if (j > i) {
+        sum += d_L(j, i) * d_x(j);
+      }
+
+      __syncwarp();
     }
-    __syncwarp();
-
-    if (j > i) {
-      sum += d_L(j, i) * d_x(j);
-    }
-
-    __syncwarp();
   }
 }
 
@@ -164,38 +229,30 @@ double matrix_solve(std::vector<Eigen::MatrixXd>& M,
 
   cudaEventRecord(start);
 
-  // Matrix Cholesky factorization
-  int offset = 0;
-  while (offset < n) {
-    cholesky_factorization<<<num_equations, 32>>>(d_M, d_L, num_equations, n,
-                                                  offset);
-    offset += 32;
+  int num_stride = (n + 31) / 32;
 
-    HANDLE_ERROR(cudaGetLastError());
-    HANDLE_ERROR(cudaDeviceSynchronize());
-  }
+  // Matrix Cholesky factorization
+
+  cholesky_factorization<<<num_equations, 32>>>(d_M, d_L, num_equations, n,
+                                                num_stride);
+
+  HANDLE_ERROR(cudaGetLastError());
+  HANDLE_ERROR(cudaDeviceSynchronize());
 
   // Matrix Cholesky solve forward
-  offset = 0;
-  while (offset < n) {
-    cholesky_solve_forward<<<num_equations, 32>>>(d_L, d_b, d_x, d_y,
-                                                  num_equations, n, offset);
-    offset += 32;
 
-    HANDLE_ERROR(cudaGetLastError());
-    HANDLE_ERROR(cudaDeviceSynchronize());
-  }
+  cholesky_solve_forward<<<num_equations, 32>>>(d_L, d_b, d_x, d_y,
+                                                num_equations, n, num_stride);
+
+  HANDLE_ERROR(cudaGetLastError());
+  HANDLE_ERROR(cudaDeviceSynchronize());
 
   // Matrix Cholesky solve backward
-  offset = 0;
-  while (offset < n) {
-    cholesky_solve_backward<<<num_equations, 32>>>(d_L, d_b, d_x, d_y,
-                                                   num_equations, n, offset);
-    offset += 32;
+  cholesky_solve_backward<<<num_equations, 32>>>(d_L, d_b, d_x, d_y,
+                                                 num_equations, n, num_stride);
 
-    HANDLE_ERROR(cudaGetLastError());
-    HANDLE_ERROR(cudaDeviceSynchronize());
-  }
+  HANDLE_ERROR(cudaGetLastError());
+  HANDLE_ERROR(cudaDeviceSynchronize());
 
   cudaEventRecord(stop);
 
@@ -208,6 +265,11 @@ double matrix_solve(std::vector<Eigen::MatrixXd>& M,
   // Copy to host
   HANDLE_ERROR(cudaMemcpy(x_result, d_x, sizeof(double) * num_equations * n,
                           cudaMemcpyDeviceToHost));
+
+  for (int i = 0; i < num_equations; ++i) {
+    Eigen::Map<Eigen::VectorXd> x_result_i(x_result + i * n, n, 1);
+    x[i] = x_result_i;
+  }
 
   return 0;
 }
